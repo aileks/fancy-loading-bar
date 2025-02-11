880d69b535e742ccb0dad4e1568482b73ba94a0a
Aaliyah Harper <aeverly14@pm.me>
2025-02-11 15:36:21 -0500

    update title & favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
-import type React from "react" // Import React
+import type React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Fancy Animated Loading Bar",
-  description: "A website with a really fancy animated loading bar",
+  title: "Waiting...",
+  description: "Loading...",
 }
 
 export default function RootLayout({
